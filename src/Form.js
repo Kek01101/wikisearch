f838480e2f5dbc1125139c8a9c09f5c5f4140b60
Nick <nicolasmkeck@gmail.com>
2022-01-09 12:39:23 +0000

    Basic functionality achieved

--- src/Form.js
+++ src/Form.js
@@ -8,53 +8,122 @@ class SearchForm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            step: 1,
             query: "",
-            subject: ""
+            subject: "",
+            search1: "",
+            search2: "",
+            search3: "",
+            search4: "",
+            search5: "",
+            sentence1: "",
+            sentence2: "",
+            sentence3: ""
         }
 
         this.handleSubjectChange = this.handleSubjectChange.bind(this)
         this.handleQueryChange = this.handleQueryChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSearchSubmit = this.handleSearchSubmit.bind(this)
+        this.handleSubjectSelection = this.handleSubjectSelection.bind(this)
     }
 
     handleSubjectChange(event) {
         this.setState({"subject": event.target.value})
     }
+
     handleQueryChange(event) {
         this.setState({"query": event.target.value})
     }
-    async handleSubmit(event) {
-        event.preventDefault()
-        const queryParam = "query=" + this.state["query"]
-        const subjectParam = "subject=" + this.state["subject"]
 
-        const url =
-            "https://wikisearch-backend.herokuapp.com/wikisearch/?" +
-            queryParam + "&" + subjectParam
+    async handleSearchSubmit(event) {
+        event.preventDefault()
+
+        const subjectParam = "subject=" + this.state["subject"]
+        const url = "https://wikisearch-backend.herokuapp.com/wikimatch/?" + subjectParam
+
 
         const request = await fetch(url)
         const response = await request.json()
-        console.log(response.sentence_1)
-        console.log(response.sentence_2)
-        console.log(response.sentence_3)
+
+        this.setState({
+            search1: response.data0, search2: response.data1, search3: response.data2,
+            search4: response.data3, search5: response.data4, step: 2
+        })
     }
 
+    async handleSubjectSelection(num) {
+        const queryParam = "query=" + this.state["query"]
+        const titleLoc = "search" + num
+        const titleParam = "title=" + this.state[titleLoc]
+        const url = "https://wikisearch-backend.herokuapp.com/wikisearch/?" + queryParam + "&" + titleParam
+
+        const request = await fetch(url)
+        const response = await request.json()
+
+        this.setState({
+            sentence1: response.sentence_1, sentence2: response.sentence_2, sentence3: response.sentence_3,
+            step: 3
+        })
+    }
 
     render() {
-        return (
-            <Form>
-                <Row>
-                    <Form.Group as={Col} controlId="subjectGroup">
-                        <Form.Control type="subject" placeholder="Enter Subject" onChange={this.handleSubjectChange}/>
-                    </Form.Group>
+        const {step, search1, search2, search3, search4, search5, sentence1, sentence2, sentence3} = this.state
+        switch (step) {
+            case 1:
+                return (
+                    <Form>
+                        <Row>
+                            <Form.Group as={Col} controlId="subjectGroup">
+                                <Form.Control type="subject" placeholder="Enter Subject"
+                                              onChange={this.handleSubjectChange}/>
+                            </Form.Group>
 
-                    <Button as={Col} variant="flat" type="submit" onClick={this.handleSubmit}>Search</Button>
-                </Row>
-                <Form.Group controlId="queryGroup">
-                    <Form.Control type="query" placeholder="Enter Query" onChange={this.handleQueryChange}/>
-                </Form.Group>
-            </Form>
-        )
+                            <Button as={Col} variant="flat" type="submit" onClick={this.handleSearchSubmit}>Search</Button>
+                        </Row>
+                        <Form.Group controlId="queryGroup">
+                            <Form.Control type="query" placeholder="Enter Query" onChange={this.handleQueryChange}/>
+                        </Form.Group>
+                    </Form>
+                )
+            case 2:
+                return (
+                    <Form>
+                        <Form.Text className="formText">
+                            We found multiple pages for your search,<br />please select the most relevant.
+                        </Form.Text>
+                        <Row>
+                            <Button variant="flat" type="button" onClick={() => this.handleSubjectSelection("1")}>{search1}</Button>
+                        </Row>
+                        <br />
+                        <Row>
+                            <Button variant="flat" type="button" onClick={() => this.handleSubjectSelection("2")}>{search2}</Button>
+                        </Row>
+                        <br />
+                        <Row>
+                            <Button variant="flat" type="button" onClick={() => this.handleSubjectSelection("3")}>{search3}</Button>
+                        </Row>
+                        <br />
+                        <Row>
+                            <Button variant="flat" type="button" onClick={() => this.handleSubjectSelection("4")}>{search4}</Button>
+                        </Row>
+                        <br />
+                        <Row>
+                            <Button variant="flat" type="button" onClick={() => this.handleSubjectSelection("5")}>{search5}</Button>
+                        </Row>
+                    </Form>
+                )
+            case 3:
+                return(
+                    <div>
+                        <p>Sentence 1: {sentence1}</p>
+                        <br />
+                        <p>Sentence 2: {sentence2}</p>
+                        <br />
+                        <p>Sentence 3: {sentence3}</p>
+                    </div>
+                )
+
+        }
     }
 }
 
